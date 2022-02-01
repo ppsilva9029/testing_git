dd50b98042cd6d144baf24658679f63d053bffcd
ppsilva9029 <ajoseeduardoa@gmail.com>
2022-01-31 19:46:01 -0700

    Funcion dia_n agregada

--- archivo.py
+++ archivo.py
@@ -4,5 +4,18 @@ def first_day(year) -> int:
     bis = year + year//4 - year//100 + year//400
     return bis % 7
 
+def dia_n(hoy, n):
+    return (hoy + n) % 7
+
+dicc_dia = {
+    0: "Domingo",
+    1: "Lunes",
+    2: "Martes",
+    3: "Miércoles",
+    4: "Jueves",
+    5: "Viernes",
+    6: "Sábado"
+}
+
 if __name__ == "__main__":
     print(first_day(2022))
