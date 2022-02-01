8d4251b910de6e222e66ff32bdd0fe01233e5c09
ppsilva9029 <ajoseeduardoa@gmail.com>
2022-01-31 23:36:16 -0700

    Creada funcion maestra en archivo_py

--- archivo.py
+++ archivo.py
@@ -1,11 +1,14 @@
 
 
-def first_day(year) -> int:
+def first_day(year: int) -> int:
     bis = year + year//4 - year//100 + year//400
     return bis % 7
 
 def dia_n(hoy, n):
     return (hoy + n) % 7
+
+def es_bis(year):
+    return (year % 400 == 0) or (year % 4 == 0) and (year % 100 != 0)
 
 dicc_dia = {
     0: "Domingo",
@@ -17,5 +20,25 @@ dicc_dia = {
     6: "Sábado"
 }
 
+meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+dias_mes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def which_day_is(day, month, year):
+    first = first_day(year)
+
+    num_of_days = 0
+    for i in range(month-1):
+        num_of_days += dias_mes[i]
+    
+    if es_bis(year) and month > 2:
+        num_of_days+=1
+    
+    num_of_days += day - 1
+    # print(first)
+    
+    day_int = (first + num_of_days) % 7
+    return dicc_dia[day_int]
+
+
 if __name__ == "__main__":
-    print(first_day(2022))
+    print(which_day_is(17, 8, 2020))
